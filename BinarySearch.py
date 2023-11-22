058e16153035e39aad9183921d2c07cbe4c0ccc9
Jerin George Mathew <127280408+JerinGM@users.noreply.github.com>
2023-11-22 19:24:15 +0530

    BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -2,14 +2,17 @@
 # input_list = [9, 8, 6, 5, 4, 3, 2, 1, 0]
 # input_list = [5, 4, 3, 2, 1]
 input_list = [10, 8, 8, 7, 7, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 0, -1, -1, -1, -4, -5, -10]
-
-query = -1
+# input_list = []
+query = -123
 
 
 def binary_search_function(input_list, query):
     found = False
     high = len(input_list) - 1
     low = 0
+    if len(input_list) == 0:
+        found = True
+        print("Empty List")
     while low <= high:
         mid = (high + low) // 2
         # print(f"Low is {low}\nHigh is {high}\n Mid is {mid}")
